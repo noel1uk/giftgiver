75625e5342180b305a59f8466e9956f011a4dc6a
Noel Vock <noel1uk@gmail.com>
2018-04-21 19:08:30 +0100

    fix: typo

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -15,5 +15,5 @@ it('initializes the `state` with an empty list of gifts', () => {
 it('adds a new gift to `state` when clicking the `add gift` button', () => {
   app.find('.btn-add').simulate('click');
 
-  expect(app.state().gifts.toEqual([{ id: 1}]));
+  expect(app.state().gifts).toEqual([{ id: 1 }]);
 });
