0cb6303cb5da091532f3aae9cf2fd30e6bf7a0b4
Noel Vock <noel1uk@gmail.com>
2018-04-30 18:04:49 +0100

    feat: add extra attributes to gift object

--- src/components/App.js
+++ src/components/App.js
@@ -35,7 +35,11 @@ class App extends Component {
           {
             this.state.gifts.map(gift => {
               return (
-                <Gift key={gift.id} />
+                <Gift 
+                  key={gift.id} 
+                  gift={gift}
+                  removeGift={this.removeGift}
+                />
               )
             })
           }
